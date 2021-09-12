d813b0d1cf4f3556ffb14ea3977754336a5e1e39
Kenichi Ooi <kenoi@jp.ibm.com>
2021-09-12 23:50:59 +0900

    Initial commit of security.auth war project

---
+++ security.auth/src/main/java/pdprof/security/auth/LogOut.java
@@ -0,0 +1,40 @@
+package pdprof.security.auth;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class logout
+ */
+@WebServlet("/logout")
+public class LogOut extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogOut() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getUserPrincipal().getName();
+		request.logout();
+		response.getWriter().append("User:").append(user).append(" is logout");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
