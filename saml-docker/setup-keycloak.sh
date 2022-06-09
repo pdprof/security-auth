923d4384e1ef2e104996eadd36a5d20915426fc5
root <root@whoop1.fyre.ibm.com>
2022-06-08 22:23:11 -0700

    Allow http for pdprof realms

--- saml-docker/setup-keycloak.sh
+++ saml-docker/setup-keycloak.sh
@@ -6,3 +6,4 @@ $KCADM create realms -s realm=pdprof -s enabled=true --no-config --server http:/
 $KCADM create users -r pdprof -s username=pdprof -s email="pdprof@must.gather" -s enabled=true --no-config --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 $KCADM set-password -r pdprof --username pdprof --new-password $KEYCLOAK_PASSWORD --no-config --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 $KCADM update realms/master -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+$KCADM update realms/pdprof -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
