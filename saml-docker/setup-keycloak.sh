a315ad63dd1812795360d13606f9e3964c5d1c0b
root <root@whoop1.fyre.ibm.com>
2022-06-08 22:01:02 -0700

    Update allow http for adminconsole

--- saml-docker/setup-keycloak.sh
+++ saml-docker/setup-keycloak.sh
@@ -5,3 +5,4 @@ KCADM=/opt/jboss/keycloak/bin/kcadm.sh
 $KCADM create realms -s realm=pdprof -s enabled=true --no-config --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 $KCADM create users -r pdprof -s username=pdprof -s email="pdprof@must.gather" -s enabled=true --no-config --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 $KCADM set-password -r pdprof --username pdprof --new-password $KEYCLOAK_PASSWORD --no-config --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+$KCADM update realms/master -s sslRequired=NONE --server http://localhost:8080/auth --realm master --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
