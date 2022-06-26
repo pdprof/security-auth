c59982a6ca676c1215314eb05a7505bcd3e3b193
Kenichi Ooi <kenoi@jp.ibm.com>
2022-06-27 08:32:21 +0900

    Update to access from remote host

--- saml-docker/setup-docker.sh
+++ saml-docker/setup-docker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-if [ ! -f trapit ]; then
-     echo "===================="
-     echo "Please download trapit from https://www.ibm.com/support/pages/node/709009 and put it on the same directory."
-     echo "===================="
-     exit 1
+#if [ ! -f trapit ]; then
+#     echo "===================="
+#     echo "Please download trapit from https://www.ibm.com/support/pages/node/709009 and put it on the same directory."
+#     echo "===================="
+#     exit 1
+#fi
+#chmod 755 trapit
+if [ -z $ACCESS_HOST ]; then
+  echo 'Please set the "ACCESS_HOST" environment variable and try again.' 
+  exit 2
 fi
-chmod 755 trapit
 mkdir -p resources/security
 
 docker build -t liberty-idp -f Dockerfile.keycloak .
@@ -13,10 +17,10 @@ docker run -d -p 8080:8080 --name kc liberty-idp
 sleep 100
 docker exec kc /tmp/setup-keycloak.sh
 curl http://localhost:8080/auth/realms/pdprof/protocol/saml/descriptor > resources/security/idpMetadata.xml
-#sed -i s/localhost:8080/idp-route-default.apps-crc.testing:8080/g resources/security/idpMetadata.xml
+sed -i s/localhost:8080/$ACCESS_HOST:8080/g resources/security/idpMetadata.xml
 docker build -t liberty-saml .
 docker run -d -p 9080:9080 -p 9443:9443 --name ol liberty-saml
 sleep 30
 curl -k https://localhost:9443/ibm/saml20/defaultSP/samlmetadata > spMetadata.xml
-#sed -i s/localhost:9443/saml-route-default.apps-crc.testing:9443/g spMetadata.xml
-echo "access to http://localhost:8080/auth/admin/master/console/#/realms/pdprof/clients and create clients with spMetadata.xml"
+sed -i s/localhost:9443/$ACCESS_HOST:9443/g spMetadata.xml
+echo "access to http://$ACCESS_HOST:8080/auth/admin/master/console/#/realms/pdprof/clients and create clients with spMetadata.xml"
