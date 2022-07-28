2567404e9b5105495a783ea5be800d9b6d6a3036
Kenichi Ooi <kenoi@jp.ibm.com>
2022-07-28 16:35:34 +0900

    Change files to add a group

--- ldap-docker/setup-docker.sh
+++ ldap-docker/setup-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! -f trapit ]; then
-     echo "===================="
-     echo "Please download trapit from https://www.ibm.com/support/pages/node/709009 and put it on the same directory."
-     echo "===================="
-     exit 1
-fi
+#if [ ! -f trapit ]; then
+#     echo "===================="
+#     echo "Please download trapit from https://www.ibm.com/support/pages/node/709009 and put it on the same directory."
+#     echo "===================="
+#     exit 1
+#fi
 chmod 755 trapit
 
 DOCKER_HOST=172.17.0.1
