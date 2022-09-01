aee4e56b84249e5ba29715efc4744985c008cd52
Kenichi Ooi <kenoi@jp.ibm.com>
2022-09-01 09:20:25 +0900

    Fix environment variable problem

--- ldap-docker/setup-docker.sh
+++ ldap-docker/setup-docker.sh
@@ -7,15 +7,15 @@
 #fi
 #chmod 755 trapit
 
-if [ -z $DOCKER_HOST ]; then
-  echo 'Please set the "DOCKER_HOST" environment variable and try again.'
+if [ -z $ACCESS_HOST ]; then
+  echo 'Please set the "ACCESS_HOST" environment variable and try again.'
   exit 2
 fi
 
 docker build -t ldap -f Dockerfile.openldap .
 docker run -d -p 1389:389 --name ldap ldap --loglevel debug
 #Change for Docker
-sed -i s/{ldap-host}/$DOCKER_HOST/g config/server.xml
+sed -i s/{ldap-host}/$ACCESS_HOST/g config/server.xml
 docker build -t auth .
 docker run -d -p 9080:9080 -p 9443:9443 --name ll auth
 #sleep 30
