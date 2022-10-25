8f361c2054bb2bface0f72140d34464d665787c6
Kenichi Ooi <kenoi@jp.ibm.com>
2022-10-26 08:58:14 +0900

    Add configuration for SP initiated SAML on tWAS

--- saml-docker/setup-was90.sh
+++ saml-docker/setup-was90.sh
@@ -6,7 +6,7 @@ fi
 
 sed s/localhost:9080/$ACCESS_HOST:7080/g was90/configureSaml.py > was90/configureSaml.py.custom
 sed -i s/localhost:8080/$ACCESS_HOST:8080/g was90/configureSaml.py.custom
-chmod 755 configureSaml.py.custom
+chmod 755 was90/configureSaml.py.custom
 
 docker build -t was90-saml -f Dockerfile.was90 .
 docker run -d -p 7060:9060 -p 7043:9043 -p 7080:9080 -p 7443:9443 --name was90-saml was90-saml
