795be2cc6fc5b1df2218a2b4d054159d1f356172
Kenichi Ooi <kenoi@jp.ibm.com>
2023-02-27 15:51:36 +0900

    Fix unrequired lines added during merge

--- saml-docker/setup-was90.sh
+++ saml-docker/setup-was90.sh
@@ -12,10 +12,7 @@ docker build -t was90-saml -f Dockerfile.was90 .
 docker run -d -p 7060:9060 -p 7043:9043 -p 7080:9080 -p 7443:9443 --name was90-saml was90-saml
 sleep 30
 docker cp was90-saml:/work/spMetadata-was90.xml .
-<<<<<<< HEAD
 docker cp was90-saml:/opt/IBM/WebSphere/AppServer/profiles/AppSrv01/config/cells/DefaultCell01/nodes/DefaultNode01/key.p12 .
-=======
 sed -i s/localhost:9080/$ACCESS_HOST:7080/g spMetadata-was90.xml
->>>>>>> master
 sed -i s/localhost:9443/$ACCESS_HOST:7443/g spMetadata-was90.xml
 echo "access to http://$ACCESS_HOST:8080/auth/admin/master/console/#/realms/pdprof/clients and create clients with spMetadata-was90.xml"
